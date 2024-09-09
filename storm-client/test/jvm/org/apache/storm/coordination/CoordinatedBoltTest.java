653cc5001dd73b5dedb7a7eafa8249fb0dfff04c
Berna1998 <francy.1927@hotmail.it>
2024-09-09 19:16:30 +0200

    Uptade of Tests

--- storm-client/test/jvm/org/apache/storm/coordination/CoordinatedBoltTest.java
+++ storm-client/test/jvm/org/apache/storm/coordination/CoordinatedBoltTest.java
@@ -1,6 +1,173 @@
 package org.apache.storm.coordination;
 
+import org.apache.storm.Constants;
+import org.apache.storm.task.GeneralTopologyContext;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.IRichBolt;
+import org.apache.storm.Config;
+import org.apache.storm.tuple.TupleImpl;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(value = Parameterized.class)
 public class CoordinatedBoltTest {
 
+    private final Map<String, Object> configurations;
+    private final String srcComp;
+    private final String streamId;
+    private final List<Object> data;
+    private final CoordinatedBolt.SourceArgs sourceArgs;
+    private final CoordinatedBolt.IdStreamSpec idStreamSpec;
+
+    public CoordinatedBoltTest(Map<String, Object> configurations, String srcComp, String streamId, List<Object> data, CoordinatedBolt.SourceArgs sourceArgs, CoordinatedBolt.IdStreamSpec idStreamSpec){
+        this.configurations = configurations;
+        this.srcComp = srcComp;
+        this.streamId = streamId;
+        this.data = data;
+        this.sourceArgs = sourceArgs;
+        this.idStreamSpec = idStreamSpec;
+    }
+
+    @Parameterized.Parameters
+    public static Collection getCoordinatedBolt() {
+
+        List<Object> data1 = new ArrayList<>();
+        data1.add("data");
+        data1.add(33);
+
+        List<Object> dataNull = new ArrayList<>();
+        dataNull.add(null);
+        dataNull.add(null);
+
+        Map<String,Object> emptyConfig = new HashMap<>();
+        Map<String,Object> configurations = new HashMap<>();
+        configurations.put(Config.TOPOLOGY_BOLTS_WINDOW_LENGTH_COUNT, 100);
+
+        return Arrays.asList(new Object[][]{
+                {configurations,"prova", "streamId", data1, CoordinatedBolt.SourceArgs.all(), CoordinatedBolt.IdStreamSpec.makeDetectSpec("prova", "streamId")},
+                {configurations,"prova", "", data1, CoordinatedBolt.SourceArgs.all(), CoordinatedBolt.IdStreamSpec.makeDetectSpec("prova", "")},
+                {configurations,"prova", Constants.COORDINATED_STREAM_ID, data1, CoordinatedBolt.SourceArgs.all(), CoordinatedBolt.IdStreamSpec.makeDetectSpec("prova", Constants.COORDINATED_STREAM_ID)},
+
+                {configurations,"prova", "streamId", data1, CoordinatedBolt.SourceArgs.single(), CoordinatedBolt.IdStreamSpec.makeDetectSpec("prova", "streamId")},
+                {configurations,"prova", "", data1, CoordinatedBolt.SourceArgs.single(), CoordinatedBolt.IdStreamSpec.makeDetectSpec("prova", "")},
+                {configurations,"prova", Constants.COORDINATED_STREAM_ID, data1, CoordinatedBolt.SourceArgs.single(), CoordinatedBolt.IdStreamSpec.makeDetectSpec("prova", Constants.COORDINATED_STREAM_ID)},
+
+                {configurations,"", "streamId", data1, CoordinatedBolt.SourceArgs.all(), CoordinatedBolt.IdStreamSpec.makeDetectSpec("", "streamId")},
+                {configurations,"", "", data1, CoordinatedBolt.SourceArgs.all(), CoordinatedBolt.IdStreamSpec.makeDetectSpec("", "")},
+                {configurations,"", Constants.COORDINATED_STREAM_ID, data1, CoordinatedBolt.SourceArgs.all(), CoordinatedBolt.IdStreamSpec.makeDetectSpec("", Constants.COORDINATED_STREAM_ID)},
+
+                {configurations,"","streamId",data1,mock(CoordinatedBolt.SourceArgs.class),CoordinatedBolt.IdStreamSpec.makeDetectSpec(null,null)},
+                {configurations,"","",data1,mock(CoordinatedBolt.SourceArgs.class),CoordinatedBolt.IdStreamSpec.makeDetectSpec(null,null)},
+                {null,"","",data1,mock(CoordinatedBolt.SourceArgs.class),CoordinatedBolt.IdStreamSpec.makeDetectSpec(null,null)},
+                {null,"prova","streamId",data1,mock(CoordinatedBolt.SourceArgs.class),null},
+                {null,"prova",Constants.COORDINATED_STREAM_ID,data1,mock(CoordinatedBolt.SourceArgs.class),CoordinatedBolt.IdStreamSpec.makeDetectSpec(null,null)},
+                {null,null, "", dataNull, CoordinatedBolt.SourceArgs.single(), null},
+
+
+                {emptyConfig,"prova","streamId",data1,mock(CoordinatedBolt.SourceArgs.class),null},
+                {emptyConfig,"prova",Constants.COORDINATED_STREAM_ID,data1,mock(CoordinatedBolt.SourceArgs.class),CoordinatedBolt.IdStreamSpec.makeDetectSpec(null,null)},
+        });
+
+    }
+
+    @Test
+    public void testClass() {
+        IRichBolt richBolt = mock(IRichBolt.class);
+
+        CoordinatedBolt coordBolt = new CoordinatedBolt(richBolt, this.srcComp, this.sourceArgs, this.idStreamSpec);
+
+        TopologyContext context = mock(TopologyContext.class);
+        OutputCollector collector = mock(OutputCollector.class);
+
+        coordBolt.prepare(this.configurations, context, collector);
+
+        GeneralTopologyContext gtc = mock(GeneralTopologyContext.class);
+
+        TupleImpl tuple = new TupleImpl(gtc, this.data, this.srcComp, 1, this.streamId);
+
+        coordBolt.execute(tuple);
+
+        if (this.idStreamSpec != null && this.idStreamSpec.id.get_streamId() != null) {
+            assertEquals(this.streamId, this.idStreamSpec.id.get_streamId());
+        }
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", 33);
+        when(richBolt.getComponentConfiguration()).thenReturn(map);
+        Assert.assertEquals(map, coordBolt.getComponentConfiguration());
+        coordBolt.cleanup();
+
+
+    }
+
+    @Test
+    public void testWithDifferentConstructor(){
+
+        IRichBolt richBolt = mock(IRichBolt.class);
+
+        CoordinatedBolt coordBolt = new CoordinatedBolt(richBolt);
+
+        TopologyContext context = mock(TopologyContext.class);
+        OutputCollector collector = mock(OutputCollector.class);
+
+        Map<String, Object> configurations = new HashMap<>();
+
+        coordBolt.prepare(configurations, context, collector);
+
+        GeneralTopologyContext gtc = mock(GeneralTopologyContext.class);
+
+        TupleImpl tuple = new TupleImpl(gtc, this.data, this.srcComp, 1, this.streamId);
+
+        coordBolt.execute(tuple);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", 33);
+        when(richBolt.getComponentConfiguration()).thenReturn(map);
+
+        Assert.assertEquals(map, coordBolt.getComponentConfiguration());
+
+        coordBolt.cleanup();
+
+    }
+
+    @Test
+    public void testForExcep(){
+        IRichBolt richBolt = mock(IRichBolt.class);
+        CoordinatedBolt coordBolt = new CoordinatedBolt(richBolt, this.srcComp, this.sourceArgs, this.idStreamSpec);
+        TopologyContext context = mock(TopologyContext.class);
+        OutputCollector collector = mock(OutputCollector.class);
+        GeneralTopologyContext gtc = mock(GeneralTopologyContext.class);
+
+        TupleImpl tuple = new TupleImpl(gtc, this.data, this.srcComp, 1, this.streamId);
+
+        try{
+            coordBolt.prepare(this.configurations, null, collector);
+
+        } catch (NullPointerException e) {
+            assertFalse(false);
+        }
+
+        try{
+            coordBolt.prepare(this.configurations, context, collector);
+            coordBolt.execute(null);
+        } catch (NullPointerException  e) {
+            assertFalse(false);
+        }
+
+        try{
+            coordBolt.prepare(this.configurations, context, null);
+            coordBolt.execute(tuple);
+        } catch (NullPointerException  e) {
+            assertFalse(false);
+        }
+    }
 
 }
--- storm-client/test/jvm/org/apache/storm/utils/ObjectReaderTest.java
+++ storm-client/test/jvm/org/apache/storm/utils/ObjectReaderTest.java
@@ -1,6 +1,183 @@
 package org.apache.storm.utils;
 
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+@RunWith(value = Parameterized.class)
 public class ObjectReaderTest {
 
+    private final Object stringColl;
+    private final Object becomeString;
+    private final Object becomeInt;
+    private final Object becomeDouble;
+    private final Object becomeLong;
+    private final Object checkBool;
+    private final int type;
+    private final Object stayString;
+
+
+    public ObjectReaderTest(int type, Object stringColl, Object becomeString, Object becomeInt, Object becomeDouble, Object becomeLong, Object checkBool, String stayStrig){
+        this.type = type;
+        this.stringColl = stringColl;
+        this.becomeString = becomeString;
+        this.becomeInt = becomeInt;
+        this.becomeDouble = becomeDouble;
+        this.becomeLong = becomeLong;
+        this.checkBool = checkBool;
+        this.stayString = stayStrig;
+    }
+
+
+
+    @Parameterized.Parameters
+    public static Collection returnParams() {
+        Collection<String> stringCollection = new ArrayList<>();
+        stringCollection.add("Hello");
+        stringCollection.add("World");
+        return Arrays.asList(new Object[][] {
+                {1,stringCollection, 44,"33",22,"11",11>12,"string"},
+                {2,33,null,null,null,null,null,""},
+                {3,null,22,(long) 33,"",3,null,""}
+
+        });
+    }
+
+    @Test
+    public void checkTests(){
+        if(this.type == 1){
+            //TEST CHE VANNO A BUON FINE
+
+            //Right List String
+            List<String> lis = new ArrayList<>();
+            lis.add("Hello");
+            lis.add("World");
+            assertEquals(lis, ObjectReader.getStrings(this.stringColl));
+
+            //Right String
+            List<String> lis2 = new ArrayList<>();
+            lis2.add("string");
+            assertEquals(lis2, ObjectReader.getStrings(this.stayString)); //SE PASSI UNA STRINGA A getStrings ti torna lista con la stringa
+
+            //Right String
+            String str = "44";
+            assertEquals(str, ObjectReader.getString(this.becomeString));
+
+            //Right String with default
+            String s = "string";
+            assertEquals(s, ObjectReader.getString(this.stayString, null));
+
+            //Right Int no default
+            int num2 = 33;
+            assertEquals(num2, ObjectReader.getInt(this.becomeInt));
+
+            //Right Int con default
+            assertEquals(num2, ObjectReader.getInt(this.becomeInt,0));
+
+            //Right Double no deafult
+            long numD = 22;
+            assertEquals(numD, ObjectReader.getDouble(this.becomeDouble));
+
+            //Right Double con deafult
+            assertEquals(numD, ObjectReader.getDouble(this.becomeDouble,0.0));
+
+            //Right Long no default
+            long numL = 11;
+            assertEquals(numL, ObjectReader.getLong(this.becomeLong));
+
+            //Right Long con default
+            assertEquals(numL, ObjectReader.getLong(this.becomeLong,(long) 0));
+
+            //Right Bool
+            assertFalse(ObjectReader.getBoolean(this.checkBool, true));
+
+        }else if(this.type == 2){
+            //TEST CHE VANNO MALE
+
+            //Valori diversi
+            //String che ritorna il defaultValue se null
+            assertEquals("default", ObjectReader.getString(this.becomeString, "default"));
+
+            //Null con Long
+            assertNull(ObjectReader.getLong(this.becomeLong));
+
+            long valLong = 0;
+            assertEquals(valLong,ObjectReader.getLong(this.becomeLong,(long)0));
+
+            //Null con bool
+            assertFalse(ObjectReader.getBoolean(this.checkBool, false));
+
+            //Eccezioni
+            //Exception con List String
+            assertThrows(Exception.class, () -> {
+                ObjectReader.getStrings(this.stringColl);
+            });
+
+            //Exception con String
+            assertThrows(Exception.class, () -> {
+                ObjectReader.getString(this.becomeString);
+            });
+
+            //Exception con String e default
+            assertThrows(Exception.class, () -> {
+                ObjectReader.getString(this.stringColl,null);
+            });
+
+            //Exception con Int
+            assertThrows(Exception.class, () -> {
+                ObjectReader.getInt(this.becomeInt);
+            });
+
+            assertEquals(0,ObjectReader.getInt(this.becomeInt,0));
+
+            //Exception con Double
+            assertThrows(Exception.class, () -> {
+                ObjectReader.getDouble(this.becomeDouble);
+            });
+
+            assertEquals(0.0,ObjectReader.getDouble(this.becomeDouble,0.0));
+
+            //Exception con Bool
+            assertThrows(Exception.class, () -> {
+                ObjectReader.getBoolean(this.stringColl,false);
+            });
+
+
+        }else{
+
+            assertFalse(ObjectReader.getBoolean(this.checkBool,false));
+
+            assertEquals(33,ObjectReader.getInt(this.becomeInt)); //passi long a get int
+
+            assertEquals(22,ObjectReader.getInt(22));  //passi int a get int
+
+            assertEquals(new ArrayList<>(),ObjectReader.getStrings(this.stringColl));   //con null a getStrings torna lista vuota
+
+
+            //Exception con valore double
+            double dov = 33.0;
+            assertThrows(Exception.class, () -> {
+                ObjectReader.getInt(dov,0);
+            });
+
+            //Exception con Double
+            assertThrows(Exception.class, () -> {
+                ObjectReader.getDouble(this.becomeDouble,0.0);
+            });
+
+
+            long val = 3;
+            assertEquals(val, ObjectReader.getLong(this.becomeLong));
+
+        }
+    }
 }
